22b400fb1db7ceed78be07b0864411f4c278801e
borab96 <borabasa@gmail.com>
2020-08-29 17:06:24 -0500

    more custom

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d68';
+const key = 'eefaaf99ae44ff872192d7db3346fecd';
 
 // Set Position function
 setPosition();
